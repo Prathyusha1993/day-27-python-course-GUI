352dfc6a0bb57ae8a9eb76e605c08e7be272f3fe
Prathyusha Kurigala <prathyushakiran@Prathyushas-MBP.attlocal.net>
2025-01-27 15:30:58 -0800

    day 27 GUI-tkinter, python challenge

--- main.py
+++ main.py
@@ -3,14 +3,34 @@ import tkinter
 window = tkinter.Tk()
 window.title('My First GUI Program')
 window.minsize(width=500, height=300)
+window.config(padx=100, pady=200)
+
+# button
+def button_clicked():
+    print('I got clicked')
+    new_text = input.get()
+    my_label.config(text=new_text)
 
 #label
 my_label = tkinter.Label(text='I Am a Label', font=('Arial', 24))
-my_label.pack(side='left')
+my_label.config(text='New Text')
+my_label.grid(column=0, row=0)
+my_label.config(padx=50, pady=50)
+
+# change or update the text
+# my_label['text'] = 'New Text'
 
 
+button = tkinter.Button(text='Click Me', command=button_clicked)
+button.grid(column=1, row=1)
 
+new_button = tkinter.Button(text='New Button', command=button_clicked)
+new_button.grid(column=2, row=0)
 
+# entry
+input = tkinter.Entry(width=10)
+print(input.get())
+input.grid(column=3, row=2)
 
 
 window.mainloop()
